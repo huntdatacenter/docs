15cee094c1c6ae3075cf2b0183939a2dc648435b
Matus Kosut <matus.kosut@ntnu.no>
2020-05-12 08:39:04 +0200

    redirect assets subdomain on pdf

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,20 @@
+
+export default ({ router }) => {
+  router.beforeEach(async (to, from, next) => {
+    // `to` and `from` are both route objects
+    // this.name = to.params.name
+    try {
+      // console.log("https://assets.hdc.ntnu.no" + to.path);
+      // console.log(to.path);
+      // console.log(to);
+      if (to.path.startsWith('/assets/') && to.path.endsWith(".pdf")) {
+        console.log("https://assets.hdc.ntnu.no" + to.path);
+        window.location.href = "https://assets.hdc.ntnu.no" + to.path;
+      } else {
+        next();
+      }
+    } catch(err) {
+      next();
+    }
+  })
+}
