496ef74e8983b400c9f84283197f8745c6d0b82a
Matus Kosut <matus.kosut@ntnu.no>
2021-10-13 12:45:16 +0200

    redirect tingweeks

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,18 +1,21 @@
-
 export default ({ router }) => {
   router.beforeEach(async (to, from, next) => {
     // `to` and `from` are both route objects
     // this.name = to.params.name
     try {
-      if (to.path.startsWith('/assets/') && to.path.endsWith(".pdf")) {
+      if (to.path.startsWith("/assets/") && to.path.endsWith(".pdf")) {
         // NOTE Redirect PDF assets if not found - fails to redirect existing
         window.location.href = "https://assets.hdc.ntnu.no" + to.path;
+        next();
+      } else if (to.path.startsWith("/tingweeks/")) {
+        // NOTE Redirect PDF assets if not found - fails to redirect existing
+        window.location.href = "https://docs.hdc.ntnu.no/tingweek/";
         next();
       } else {
         next();
       }
-    } catch(err) {
+    } catch (err) {
       next();
     }
-  })
-}
+  });
+};
