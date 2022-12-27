6a78911b21ba57f515dc840157d74cdfd8ef33e3
Oddgeir Lingaas Holmen <oddgeir.lingaas.holmen@ntnu.no>
2022-12-27 17:01:05 +0100

    sanity

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -66,12 +66,20 @@ module.exports = {
         link: "/"
       },
       {
-        text: "Getting started",
-        link: "/getting-started/"
+        text: "Do science",
+        link: "/do-science/"
+      },
+      {
+        text: "Administer science",
+        link: "/administer-science/"
+      },
+      {
+        text: "Govern science",
+        link: "/govern-science/"
       }
     ],
     navbar: [{
-        text: "Home",
+        text: "Home - test1",
         link: "/"
       },
       {
@@ -81,275 +89,21 @@ module.exports = {
     ],
     // https://v1.vuepress.vuejs.org/theme/default-theme-config.html#sidebar
     sidebar: {
-      "/": [{
-          title: "Home",
-          collapsable: false,
-          text: "Home",
-          isGroup: true,
-          children: ["/",
-            "/system-status.md",
-            "/contribute.md",
-            "/contact.md",
-          ]
-        },
-        {
-          title: "About",
-          text: "About",
-          isGroup: true,
-          children: [
-            "/about/vision.md",
-            "/about/team.md",
-            "/about/activities.md",
-            "/about/art/",
-            "/about/affiliation.md",
-          ]
-        },
-        {
-          title: "Governance",
-          text: "Governance",
-          isGroup: true,
-          children: [
-            "/governance/",
-            "/governance/tingweek.md",
-            "/governance/lagtun.md",
-            "/governance/resolutions.md",
-            "/governance/policies.md",
-            "/governance/certificates.md",
-            "/governance/subcontractors.md",
-            "/governance/responsibilities.md",
-          ]
-        },
-        {
-          title: "Coordination",
-          text: "Coordination",
-          isGroup: true,
-          children: [
-            "/coordination/",
-            "/coordination/services-model.md",
-            {
-              title: "Get going",
-              text: "Get going",
-              isGroup: true,
-              children: [
-                "/coordination/get-going/",
-                "/coordination/get-going/data-controller.md",
-                "/coordination/get-going/service-center.md",
-                "/coordination/get-going/data-space.md",
-                "/coordination/get-going/lab.md",
-              ]
-            },
-            {
-              title: "Agreements",
-              text: "Agreements",
-              isGroup: true,
-              children: [
-                "/coordination/agreements/overview.md",
-                "/coordination/agreements/downloads.md",
-                "/coordination/agreements/faq.md",
-              ]
-            },
-            {
-              title: "Services",
-              text: "Services",
-              isGroup: true,
-              children: [
-                "/coordination/services/overview.md",
-                "/coordination/services/specifications.md",
-                "/coordination/services/machine-types.md",
-                "/coordination/services/responsiveness.md",
-              ]
-             },
-            {
-              title: "Prices",
-              text: "Prices",
-              isGroup: true,
-              children: [
-                "/coordination/prices/",
-                "/coordination/prices/pricelist.md",
-                "/coordination/prices/calculator.md",
-                "/coordination/prices/examples.md",
-                "/coordination/prices/faq.md",
-              ]
-             },
-            {
-              title: "Risk",
-              text: "Risk",
-              isGroup: true,
-              children: [
-                "/coordination/risk/",
-                "/coordination/risk/resources.md",
-                "/coordination/risk/faq.md",
-              ]
-             },
-            {
-              title: "Data",
-              text: "Data",
-              isGroup: true,
-              children: [
-                "/coordination/data/",
-                "/coordination/data/faq.md",
-              ]
-             },
-            "/coordination/roles.md",
-            "/coordination/faq.md",
-           ]
-        },
-        {
-          title: "Getting started",
-          text: "Getting started",
-          isGroup: true,
-          children: [
-            "/getting-started/",
-            "/getting-started/1-collect-keys.md",
-            "/getting-started/2-configure-vpn.md",
-            "/getting-started/3-configure-ssh.md",
-          ]
-        },
-        {
-          title: "Working in your lab",
-          text: "Working in your lab",
-          isGroup: true,
-          children: [
-            "/working-in-your-lab/",
-            "/working-in-your-lab/community.md",
-            {
-              title: "Workbench",
-              text: "Workbench",
-              isGroup: true,
-              children: [
-                "/working-in-your-lab/workbench/",
-                "/working-in-your-lab/workbench/installation.md",
-                "/working-in-your-lab/workbench/principles.md",
-                "/working-in-your-lab/workbench/faq.md",
-                "/working-in-your-lab/workbench/troubleshooting.md",
-              ]
-            },
-            {
-              title: "Technical tools",
-              text: "Technical tools",
-              isGroup: true,
-              children: [
-                "/working-in-your-lab/technical-tools/",
-                "/working-in-your-lab/technical-tools/git.md",
-                "/working-in-your-lab/technical-tools/gpu.md",
-                "/working-in-your-lab/technical-tools/htop.md",
-                "/working-in-your-lab/technical-tools/mobaxterm.md",
-                "/working-in-your-lab/technical-tools/ncdu.md",
-                "/working-in-your-lab/technical-tools/singularity.md",
-                "/working-in-your-lab/technical-tools/docker.md",
-                "/working-in-your-lab/technical-tools/terminal-multiplexers.md",
-                "/working-in-your-lab/technical-tools/x2go.md",
-                "/working-in-your-lab/technical-tools/vnc.md",
-                "/working-in-your-lab/technical-tools/client-certificate.md",
-              ]
-            },
-            {
-              title: "Analytical tools",
-              text: "Analytical tools",
-              isGroup: true,
-              children: [
-                "/working-in-your-lab/analytical-tools/",
-                "/working-in-your-lab/analytical-tools/bluebox.md",
-                "/working-in-your-lab/analytical-tools/conda.md",
-                "/working-in-your-lab/analytical-tools/epacts.md",
-                "/working-in-your-lab/analytical-tools/fsl.md",
-                "/working-in-your-lab/analytical-tools/freesurfer.md",
-                "/working-in-your-lab/analytical-tools/jupyter-lab.md",
-                "/working-in-your-lab/analytical-tools/matlab.md",
-                "/working-in-your-lab/analytical-tools/plink.md",
-                "/working-in-your-lab/analytical-tools/pycharm.md",
-                "/working-in-your-lab/analytical-tools/r-studio.md",
-                "/working-in-your-lab/analytical-tools/regenie.md",
-                "/working-in-your-lab/analytical-tools/saige.md",
-                "/working-in-your-lab/analytical-tools/spyder-ide.md",
-                "/working-in-your-lab/analytical-tools/stata.md",
-              ]
-            },
-            {
-              title: "Transfer tools",
-              text: "Transfer tools",
-              isGroup: true,
-              children: [
-                "/working-in-your-lab/transfer-tools/",
-                "/working-in-your-lab/transfer-tools/7z.md",
-                "/working-in-your-lab/transfer-tools/lftp.md",
-                "/working-in-your-lab/transfer-tools/rsync.md",
-                "/working-in-your-lab/transfer-tools/sshfs.md",
-                "/working-in-your-lab/transfer-tools/winscp.md",
-              ]
-            },
-            {
-              title: "Guides",
-              text: "Guides",
-              isGroup: true,
-              children: [
-                "/working-in-your-lab/guides/workbench-stata.md"
-              ],
-            },
-          ]
-        },
-        {
-          title: "Data transfers",
-          text: "Data transfers",
-          isGroup: true,
-          children: [
-            "/data-transfers/",
-            "/data-transfers/internal-kista.md",
-            "/data-transfers/external-kista.md",
-          ],
-        },
-        {
-          title: "Service desk",
-          text: "Service desk",
-          isGroup: true,
-          children: [
-            "/service-desk/",
-            "/service-desk/user-orders.md",
-            "/service-desk/lab-orders.md",
-            "/service-desk/data-space-orders.md",
-            "/service-desk/service-center-orders.md",
-            "/service-desk/data-controller-orders.md",
-            {
-              title: "Access guides",
-              text: "Access guides",
-              isGroup: true,
-              children: [
-                "/service-desk/access-guides/reset-gauth.md",
-                "/service-desk/access-guides/reset-ssh.md",
-                "/service-desk/access-guides/reset-vpn.md",
-                "/service-desk/access-guides/workbench-reissue.md",
-                "/service-desk/access-guides/workbench-request.md",
-              ],
-            },
-          ]
-        },
-        {
-          title: "Troubleshooting",
-          text: "Troubleshooting",
-          isGroup: true,
-          children: [
-            "/troubleshooting/",
-            "/troubleshooting/connection.md",
-            "/troubleshooting/lab-activities.md",
-            "/troubleshooting/labreports.md",
-          ],
-        },
-        {
-          title: "FAQ",
-          text: "FAQ",
-          isGroup: true,
-          children: [
-            "/faq/",
-            "/faq/users.md",
-            "/faq/storage.md",
-            "/faq/compute.md",
-            "/faq/internal-transfer.md",
-            "/faq/external-transfer.md",
-            "/faq/security.md",
-          ]
-        }
+      "/do-science/": [
+        "/do-science/getting-started/",
+      ],
+      "/administer-science/": [
+        "/administer-science/"
+      ],
+      "/govern-science/": [
+        "/govern-science/"
+      ],
+      "/": [
+        "",
+        "contact", 
+        "system-status"
       ]
-    }
+   },
   },
   chainWebpack: (config) => {
     config.module.rule("images").use("url-loader").options({
---
+++ docs/do-science/README.md
@@ -0,0 +1,25 @@
+---
+title: Welcome
+category: HUNT Cloud
+readingTime: null
+description: HUNT Cloud is a scientific computing service located at NTNU in Norway. These pages contains official user documentation on our cloud services. Their content is aimed at lab users.
+---
+
+# Welcome to the HUNT Cloud Documentation
+
+HUNT Cloud is a scientific computing environment located at [NTNU](https://www.ntnu.edu/) in Norway.
+
+These pages contains official user documentation on our services. The content is aimed at lab users and data space coordinators.
+
+Read our [eBook](https://assets.hdc.ntnu.no/assets/ebook-hunt-cloud-services.pdf) and visit our [HUNT Cloud](https://www.ntnu.edu/mh/huntcloud) pages for general information on our services, and see our [Ting reports](/tingweek/#reports) for detailed updates on our activities.
+
+::: tip Next Ting week
+
+- **Lab ting**: Tuesday 14 February 2023 - 2 to 4 p.m. CET
+- **Data space ting**: Wednesday 15 February 2023 - 9 to 11 a.m. CET
+- **Data controller ting**: Wednesday 15 February 2023 - 1 to 3 p.m. CET
+- **Lagtun**: Thursday 16 February 2023 - 9 to 11 a.m. CET
+
+[Read more about the Tingweeks](tingweek/)
+
+:::
