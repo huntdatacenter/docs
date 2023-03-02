0fca9df13b2df58cb04ffe4e0a3f60b47ccd92a5
Matus Kosut <matus.kosut@ntnu.no>
2023-03-02 20:29:13 +0100

    mobaxterm and workbench

--- docs/.vuepress/components/SSHConfig.vue
+++ docs/.vuepress/components/SSHConfig.vue
@@ -1,6 +1,7 @@
 <script>
 import {
   VApp,
+  VBtn,
   VCol,
   VRow,
   VTextField,
@@ -16,6 +17,7 @@ export default {
   name: "SSHConfig",
   components: {
     VApp,
+    VBtn,
     VCol,
     VRow,
     VTextField,
@@ -33,6 +35,7 @@ export default {
     return {
       query: {
         ipAddress: null,
+        // homeIpAddress: null,
         labName: null,
         username: null,
       },
@@ -52,15 +55,31 @@ Host {lab_name}
     };
   },
   computed: {
-    show() {
+    cfgShow() {
       return this.query.ipAddress && this.query.labName && this.query.username ? true : false;
     },
     configText() {
       return this.query.ipAddress && this.query.labName && this.query.username ? this.wrap(this.template) : null;
     },
     puttyHostName() {
-      return this.query.ipAddress && this.query.username ? `${this.query.username}@${this.query.ipAddress}` : null
-    }
+      return this.query.ipAddress && this.query.username ? `${this.query.username}@${this.query.ipAddress}` : null;
+    },
+    hostsWorkbench() {
+      return this.query.ipAddress && this.query.labName ? `${this.query.ipAddress}    ${this.query.labName}.lab.hdc.ntnu.no` : null;
+    },
+    entryIpUrl() {
+      return this.query.ipAddress ? this.query.ipAddress.replace('.', '%2E') : '';
+    },
+    homeIpUrl() {
+      // return '10.5.5.12'.replace('.', '%2E');
+      return 'home';
+    },
+    mobaxtermTerminal() {
+      return this.cfgShow ? `mobaxterm:${this.query.labName}%3D%23109%230%25${this.homeIpUrl}%2522%25${this.query.username}%25%250%250%25%25${this.entryIpUrl}%2522%25${this.query.username}%25%2D1%250%250%25%5FUSERPROFILE%5F%5C%2Essh%5Cid%5Frsa%25%5FUSERPROFILE%5F%5C%2Essh%5Cid%5Frsa%25%2D1%250%250%250%25%251080%25%250%250%251%23MobaFont%2510%250%250%25%2D1%2515%25236%2C236%2C236%2530%2C30%2C30%25180%2C180%2C192%250%25%2D1%250%25%25xterm%2D256color%25%2D1%25%2D1%25%5FStd%5FColors%5F0%5F%2580%2524%250%251%25%2D1%25%3Cnone%3E%25%250%250%25%2D1%230%23%20%23%2D1` : null;
+    },
+    mobaxtermRdp() {
+      return this.cfgShow ? `mobaxterm:${this.query.labName}%2DRDP%3D%2391%234%25${this.query.labName}%2D${this.homeIpUrl}%253389%25${this.query.username}%250%250%250%250%25%2D1%250%250%25%2D1%25xfce4%2Dsession%25${this.entryIpUrl}%5F%5FPIPE%5F%5F${this.homeIpUrl}%2522%5F%5FPIPE%5F%5F22%25${this.query.username}%5F%5FPIPE%5F%5F${this.query.username}%250%250%25%5FProfileDir%5F%5C%2Essh%5Cid%5Frsa%5F%5FPIPE%5F%5F%5FProfileDir%5F%5C%2Essh%5Cid%5Frsa%25%2D1%25%25%2D1%25%2D1%250%250%250%25%2D1%23MobaFont%2510%250%250%25%2D1%2515%25236%2C236%2C236%2530%2C30%2C30%25180%2C180%2C192%250%25%2D1%250%25%25xterm%25%2D1%25%2D1%25%5FStd%5FColors%5F0%5F%2580%2524%250%251%25%2D1%25%3Cnone%3E%25%250%250%25%2D1%230%23%20%23%2D1` : null;
+    },
   },
   mounted() {},
   created() {
@@ -73,6 +92,11 @@ Host {lab_name}
         this.query.ipAddress = access[0]
         this.query.labName = access[1]
         this.query.username = access[2]
+    } else if (access.length == 4) {
+        this.query.ipAddress = access[0]
+        // this.query.homeIpAddress = access[1]
+        this.query.labName = access[2]
+        this.query.username = access[3]
     }
     console.log(this.query);
   },
@@ -92,7 +116,7 @@ Host {lab_name}
 <template>
     <div class="vuewidget vuewrapper" data-vuetify>
         <v-app :id="id">
-            <v-card v-if="show" class="pt-4">
+            <v-card v-show="cfgShow" class="pt-4">
                 <v-row class="ml-3 mb-2">
                     <v-col cols="10">
                         <v-text-field
@@ -166,24 +190,74 @@ Host {lab_name}
                                 <h3 id="ssh-config"><a href="#ssh-config" class="header-anchor">#</a> SSH Config</h3>
                             </v-expansion-panel-header>
                             <v-expansion-panel-content class="mt-2">
-                                <v-textarea
-                                    v-model.trim="configText"
-                                    label="SSH Config file"
-                                    placeholder="Your link is missing access token"
-                                    persistent-placeholder
-                                    class="py-2 mt-2"
-                                    outlined
-                                    readonly
-                                    rows="13"
-                                    hide-details
-                                    @focus="$event.target.select()"
-                                ></v-textarea>
+                                <v-col cols="10">
+                                    <v-textarea
+                                        v-model.trim="configText"
+                                        label="SSH Config file"
+                                        placeholder="Your link is missing access token"
+                                        persistent-placeholder
+                                        class="py-2 mt-2"
+                                        outlined
+                                        readonly
+                                        rows="13"
+                                        hide-details
+                                        @focus="$event.target.select()"
+                                    ></v-textarea>
+                                </v-col>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                        <v-expansion-panel>
+                            <v-expansion-panel-header :disable-icon-rotate="formFilled">
+                                <h3 id="workbench"><a href="#workbench" class="header-anchor">#</a> Workbench</h3>
+                            </v-expansion-panel-header>
+                            <v-expansion-panel-content class="mt-2">
+                                <v-col cols="10">
+                                    <v-text-field
+                                        v-model="hostsWorkbench"
+                                        autocomplete="ignore-field"
+                                        label="Workbench - Hosts"
+                                        placeholder="Your link is missing access token"
+                                        persistent-placeholder
+                                        outlined
+                                        dense
+                                        readonly
+                                        hide-details
+                                        @focus="$event.target.select()"
+                                    ></v-text-field>
+                                </v-col>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                        <v-expansion-panel>
+                            <v-expansion-panel-header :disable-icon-rotate="formFilled">
+                                <h3 id="mobaxterm"><a href="#mobaxterm" class="header-anchor">#</a> MobaXterm (Windows)</h3>
+                            </v-expansion-panel-header>
+                            <v-expansion-panel-content class="mt-2">
+                                <v-col cols="10">
+                                    <v-btn
+                                     color="success"
+                                     block
+                                     target="_blank"
+                                     :href="mobaxtermTerminal"
+                                    >
+                                        Create terminal session
+                                    </v-btn>
+                                </v-col>
+                                <v-col cols="10">
+                                    <v-btn
+                                     color="primary"
+                                     block
+                                     target="_blank"
+                                     :href="mobaxtermRdp"
+                                    >
+                                        Create remote desktop session
+                                    </v-btn>
+                                </v-col>
                             </v-expansion-panel-content>
                         </v-expansion-panel>
                     </v-expansion-panels>
                 </v-card>
             </v-card>
-            <v-card v-else class="pt-4">
+            <v-card v-show="!cfgShow" class="pt-4">
                 Invalid link. Please check your emails to find the full link.
             </v-card>
         </v-app>
