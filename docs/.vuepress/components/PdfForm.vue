457bc42b282669f7f280b767918fb079b1c5e922
Matus Kosut <matus.kosut@ntnu.no>
2023-04-04 12:11:45 +0200

    draft pdf filling

--- docs/.vuepress/components/PdfForm.vue
+++ docs/.vuepress/components/PdfForm.vue
@@ -1,12 +1,17 @@
 <script>
+const fs = require("fs")
+import fetch from 'node-fetch'
+import { PDFDocument } from 'pdf-lib'
+
 import {
   VApp,
   VCol,
   VRow,
   VCard,
+  VForm,
   VBtn,
   VTextField,
-} from "vuetify/lib";
+} from "vuetify/lib"
 
 export default {
   name: "PdfForm",
@@ -16,59 +21,131 @@ export default {
     VCol,
     VRow,
     VCard,
+    VForm,
     VBtn,
     VTextField,
   },
   props: {
     id: { type: String, default: "applet" },
+    url: { type: String, default: null },
     label: { type: String, default: null },
   },
   data() {
     return {
-      username: null
-    };
+      formFields: {
+        fullName: null
+      },
+      pdfDoc: null,
+      pdfFields: [],
+      showPdf: false,
+    }
   },
   computed: {
-    // Add code
+    // Getters
   },
   mounted() {
-    // Add code
+    // Run code when component is mounted
   },
   created() {
-    // Add code
+    // Run code when component is created
+    console.log(this.url)
+
+    if (this.url) {
+      this.loadPdf(this.url)
+    }
   },
   methods: {
-    // Add code
+    // Functions that can be called with parameters
+    loadPdf(url) {
+      try {
+        fetch(url)
+          .then((response) => {
+            if (!response.ok) throw new Error(`unexpected response ${response.statusText}`)
+            return response.arrayBuffer()
+          })
+          .then((buffer) => {
+            this.pdfBuffer = buffer
+            try {
+              const read_buf = this.pdfBuffer
+              PDFDocument.load(read_buf)
+                .then((doc) => {
+                  this.pdfDoc = doc
+                  console.log(doc)
+                  this.pdfFields = doc.getForm()
+                  console.log(this.pdfFields)
+                })
+            } catch (error) {
+              console.log('Failed to read fields')
+              console.log(error)
+            }
+          })
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    submitForm() {
+      console.log(this.formFields)
+      try {
+        const read_buf = this.pdfBuffer
+        PDFDocument.load(read_buf)
+          .then((pdfDoc) => {
+            const form = pdfDoc.getForm()
+            const nameField = form.getTextField('labuserfullname')
+            nameField.setText(this.formFields.fullName)
+            pdfDoc.saveAsBase64({ dataUri: true }).then((data) => {
+              const pdfDataUri = data
+              document.getElementById('pdf').src = pdfDataUri
+              this.showPdf = true
+            })
+          })
+      } catch (error) {
+        console.log(error)
+      }
+    },
   },
-};
+}
 </script>
 
 <!-- HTML goes inside v-app -->
 <template>
   <div class="vuewidget vuewrapper" data-vuetify>
     <v-app :id="id">
-      <v-card class="pt-4">
-        <v-row class="ml-3 mb-2">
-          <v-col cols="12">
-            <v-text-field
-             v-model="username"
-             ref="username"
-             autocomplete="ignore-field"
-             label="Username"
-             placeholder=""
-             persistent-placeholder
-             outlined
-             dense
-             readonly
-             hide-details
-             @focus="$event.target.select()"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="12">
-            <v-btn>{{ label }}</v-btn>
-          </v-col>
-        </v-row>
+      <v-card v-show="showPdf" class="pt-4">
+        <!--  class="customiframe" frameBorder="0" -->
+        <iframe id="pdf" title="Agreement" style="width: 100%; height: 100%; min-height: 1000px; min-width: 960px;"></iframe>
       </v-card>
+      <v-row align="center" justify="center">
+        <v-col cols="6">
+          <v-card v-show="!showPdf" class="pt-4">
+            <v-row align="center" justify="center" style="padding-left: 36px; padding-right:36px; margin-bottom: 24px;">
+              <v-col cols="12">
+                <b>PDF Form</b>
+              </v-col>
+            </v-row>
+            <v-form @submit.prevent>
+              <v-row class="ml-3 mb-2" style="padding-left: 24px; padding-right:24px;">
+                <v-col cols="12">
+                  <v-text-field
+                  v-model="formFields.fullName"
+                  ref="fullName"
+                  autocomplete="ignore-field"
+                  label="Full name"
+                  placeholder=""
+                  persistent-placeholder
+                  outlined
+                  dense
+                  hide-details
+                  @focus="$event.target.select()"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-btn type="submit" block class="mt-2" @click="submitForm">{{ label }}</v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-app>
   </div>
 </template>
--- docs/administer-science/pdf.md
+++ docs/administer-science/pdf.md
@@ -2,12 +2,29 @@
 title: PDF Form
 category: Administer science
 permalink: /administer-science/pdf-form/
-sidebarDepth: 0
-description: PDF Form.
+# sidebarDepth: 0
+pageClass: custom-homepage
+sidebar: false
+next: false
+editLink: false
+lastUpdated: false
+readingTime: null
+tagline: null
+description: null
+footerHtml: false
 ---
-
-# PDF Form
 
 <!-- Local testing with direct URL: http://localhost:8080/administer-science/pdf-form/ -->
 
-<PdfForm label="Submit button" />
+<PdfForm label="Submit button" url="https://assets.hdc.ntnu.no/assets/agreements/hunt-cloud-user-agreement.pdf" />
+
+
+
+<style scoped>
+
+.theme-default-content {
+  max-width: none !important;
+  padding: 0 !important;
+}
+
+</style>
