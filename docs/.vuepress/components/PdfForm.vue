1883bf02118f8632369673193a777ced9b797aef
Matus Kosut <matus.kosut@ntnu.no>
2023-04-04 10:05:27 +0200

    add component with testing page

---
+++ docs/.vuepress/components/PdfForm.vue
@@ -0,0 +1,101 @@
+<script>
+import {
+  VApp,
+  VCol,
+  VRow,
+  VCard,
+  VBtn,
+  VTextField,
+} from "vuetify/lib";
+
+export default {
+  name: "PdfForm",
+  // Components need to be imported above
+  components: {
+    VApp,
+    VCol,
+    VRow,
+    VCard,
+    VBtn,
+    VTextField,
+  },
+  props: {
+    id: { type: String, default: "applet" },
+    label: { type: String, default: null },
+  },
+  data() {
+    return {
+      username: null
+    };
+  },
+  computed: {
+    // Add code
+  },
+  mounted() {
+    // Add code
+  },
+  created() {
+    // Add code
+  },
+  methods: {
+    // Add code
+  },
+};
+</script>
+
+<!-- HTML goes inside v-app -->
+<template>
+  <div class="vuewidget vuewrapper" data-vuetify>
+    <v-app :id="id">
+      <v-card class="pt-4">
+        <v-row class="ml-3 mb-2">
+          <v-col cols="12">
+            <v-text-field
+             v-model="username"
+             ref="username"
+             autocomplete="ignore-field"
+             label="Username"
+             placeholder=""
+             persistent-placeholder
+             outlined
+             dense
+             readonly
+             hide-details
+             @focus="$event.target.select()"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-btn>{{ label }}</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-app>
+  </div>
+</template>
+
+<!-- SASS is auto-translated in CSS style -->
+<style lang="sass">
+.vuewidget
+
+  &.vuewrapper
+    // reset full view - no scroll bars, no full view
+    overflow: inherit
+
+    .v-application--wrap
+      display: block
+      flex: inherit
+      min-height: initial
+      min-width: inherit
+      width: 100%
+      overflow-x: hidden
+
+.last-updated
+  display: none
+
+.language-text
+  display: flex
+  padding-top: 8px !important
+  padding-bottom: 8px !important
+
+</style>
+
---
+++ docs/administer-science/pdf.md
@@ -0,0 +1,13 @@
+---
+title: PDF Form
+category: Administer science
+permalink: /administer-science/pdf-form/
+sidebarDepth: 0
+description: PDF Form.
+---
+
+# PDF Form
+
+<!-- Local testing with direct URL: http://localhost:8080/administer-science/pdf-form/ -->
+
+<PdfForm label="Submit button" />
