a709a7acd1aa3471f8135474b9a104a7ebd47201
Matus Kosut <matus.kosut@ntnu.no>
2023-10-10 18:19:59 +0200

    update draft

--- docs/.vuepress/components/ConsentForm.vue
+++ docs/.vuepress/components/ConsentForm.vue
@@ -1,10 +1,10 @@
 <script>
 import {
   VApp,
-  VDialog,
   VBtn,
   VCol,
   VRow,
+  VSheet,
   VCard,
   VCardText,
   VCardActions,
@@ -15,10 +15,10 @@ export default {
   name: "ConsentForm",
   components: {
     VApp,
-    VDialog,
     VBtn,
     VCol,
     VRow,
+    VSheet,
     VCard,
     VCardText,
     VCardActions,
@@ -26,22 +26,74 @@ export default {
   },
   props: {
     id: { type: String, default: "applet" },
+    service: { type: String, default: "consent" },
+    apiVersion: { type: String, default: "v1" },
   },
   data() {
     return {
-      dialog: false,
-      autoUpdate: false,
-      autoUpdate1: false,
-      isUpdating: false,
-      data: {},
+      domain: 'hdc.ntnu.no',
+      isLoading: true,
+      consentToken: null,
+      form: {
+        slack: true,
+        tingweek: true,
+      },
     };
   },
-  computed: {},
-  mounted() {},
-  created() {},
+  computed: {
+    showConsent() {
+      return !this.isLoading && this.consentToken ? true : false
+    },
+    url() {
+      return `https://${this.service}-api.${this.domain}/api/${this.apiVersion}/consent/${this.consentToken}`
+    },
+  },
+  mounted() {
+    this.isLoading = false
+  },
+  created() {
+    this.isLoading = true
+    this.consentToken = this.$route.query.token ? atob(this.$route.query.token) : null
+    // console.log(this.consentToken)
+    // TODO fetch data of consent from API
+    if (this.consentToken) {
+      this.getConsentData()
+    }
+    // TODO load data from api into form variables based on main status (if PENDING keep true)
+  },
   methods: {
+    getConsentData() {
+      fetch(this.url, {
+        method: "GET",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }).then(
+        (response) => {
+          return response.text()
+        }
+      ).then((data) => {
+        console.log(data)
+        // this.data = data
+      })
+    },
     submit() {
-      console.log('submitted')
+      console.log(this.form)
+      fetch(this.url, {
+        method: "PUT",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }).then(
+        (response) => {
+          return response.text()
+        }
+      ).then((data) => {
+        console.log(data)
+        // this.data = data
+      })
     },
   },
 };
@@ -50,80 +102,89 @@ export default {
 <template>
   <div class="vuewidget vuewrapper" data-vuetify>
     <v-app :id="id">
-      <form ref="form" @submit.prevent="submit">
-        <v-row>
-          <v-col>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae lorem fermentum, 
-            ultrices metus non, sagittis est. Pellentesque habitant morbi tristique senectus et netus 
-            et malesuada fames ac turpis egestas. Phasellus vitae porta dolor. Pellentesque feugiat quis 
-            sapien et tincidunt. Nam pellentesque quam eget augue finibus pharetra. 
-            Nunc eget tortor eu libero porttitor venenatis. Nulla vehicula mollis massa. 
-            Sed dignissim mauris ac odio porttitor pulvinar.
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            Proin rhoncus eros purus, vel dapibus nisi euismod at. Donec viverra elementum vestibulum. 
-            Fusce tristique at enim quis semper. Duis vel rutrum augue. Maecenas ultricies ipsum ex, 
-            eu tristique ipsum cursus in. Pellentesque ac dictum libero. Vivamus finibus rhoncus neque non 
-            tristique. Proin volutpat purus ipsum, sed vehicula felis egestas sed.
-          </v-col>
-        </v-row>
-        <v-row justify="center">
-          <v-col cols="4">
-            <v-btn
-              href="#"
-              target="_blank"
-              color="link"
-              block
-            >
-              Privacy statement (PDF)
-            </v-btn>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="2">
-            <v-switch
-              v-model="autoUpdate"
-              :disabled="isUpdating"
-              class="mt-0"
-              color="green lighten-2"
-              hide-details
-              label="Slack"
-            ></v-switch>
-          </v-col>
-          <v-col cols="10">
-            Donec at metus suscipit nunc placerat ornare egestas venenatis metus. 
-            Cras pretium urna justo, a auctor enim sodales ut.
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="2">
-            <v-switch
-              v-model="autoUpdate1"
-              :disabled="isUpdating"
-              class="mt-0"
-              color="green lighten-2"
-              hide-details
-              label="Tingweek"
-            ></v-switch>
-          </v-col>
-          <v-col cols="10">
-            Duis vel rutrum augue. Maecenas ultricies ipsum ex, eu tristique ipsum cursus in.
-          </v-col>
-        </v-row>
-        <v-row class="mb-2" justify="center">
-          <v-col cols="2">
-            <v-btn
-              type="submit"
-              color="success"
-              block
-            >
-              Submit
-            </v-btn>
-          </v-col>
-        </v-row>
-      </form>
+      <v-row v-if="isLoading" justify="center">
+        <v-col cols="8">
+          Loading...
+        </v-col>
+      </v-row>
+      <v-row v-if="!isLoading && !showConsent" justify="center">
+        <v-col cols="8">
+          If you do not have your Consent link request one in Service desk.
+        </v-col>
+      </v-row>
+      <v-sheet class="pa-4">
+        <form v-if="showConsent" ref="form" @submit.prevent="submit">
+          <v-row>
+            <v-col>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae lorem fermentum, 
+              ultrices metus non, sagittis est. Pellentesque habitant morbi tristique senectus et netus 
+              et malesuada fames ac turpis egestas. Phasellus vitae porta dolor. Pellentesque feugiat quis 
+              sapien et tincidunt. Nam pellentesque quam eget augue finibus pharetra. 
+              Nunc eget tortor eu libero porttitor venenatis. Nulla vehicula mollis massa. 
+              Sed dignissim mauris ac odio porttitor pulvinar.
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              Proin rhoncus eros purus, vel dapibus nisi euismod at. Donec viverra elementum vestibulum. 
+              Fusce tristique at enim quis semper. Duis vel rutrum augue. Maecenas ultricies ipsum ex, 
+              eu tristique ipsum cursus in. Pellentesque ac dictum libero. Vivamus finibus rhoncus neque non 
+              tristique. Proin volutpat purus ipsum, sed vehicula felis egestas sed.
+            </v-col>
+          </v-row>
+          <v-row justify="center">
+            <v-col cols="4">
+              <v-btn
+                href="#"
+                target="_blank"
+                color="link"
+                block
+              >
+                Privacy statement (PDF)
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-switch
+                v-model="form['slack']"
+                :disabled="isLoading"
+                class="mt-0"
+                color="green lighten-1"
+                inset
+                hide-details
+                name="slack"
+                label="Slack: Donec at metus suscipit nunc placerat ornare egestas venenatis metus. Cras pretium urna justo, a auctor enim sodales ut."
+              ></v-switch>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-switch
+                v-model="form['tingweek']"
+                :disabled="isLoading"
+                class="mt-0"
+                color="green lighten-1"
+                inset
+                hide-details
+                name="tingweek"
+                label="Tingweek: Duis vel rutrum augue. Maecenas ultricies ipsum ex, eu tristique ipsum cursus in."
+              ></v-switch>
+            </v-col>
+          </v-row>
+          <v-row class="mb-2" justify="center">
+            <v-col cols="3">
+              <v-btn
+                type="submit"
+                color="success"
+                block
+              >
+                Submit consent
+              </v-btn>
+            </v-col>
+          </v-row>
+        </form>
+      </v-sheet>
     </v-app>
   </div>
 </template>
@@ -149,5 +210,8 @@ export default {
 .v-chip.v-size--small
     margin-top: 8px !important
     margin-bottom: 5px !important
+
+.last-updated
+  display: none
 
 </style>
