ea5e81d9af112ff91d5dd3809f64f9ee0cbfd971
Matus Kosut <matus.kosut@ntnu.no>
2025-03-29 09:58:25 +0100

    update ip fetch

--- docs/.vuepress/components/MyIPAddress.vue
+++ docs/.vuepress/components/MyIPAddress.vue
@@ -18,7 +18,7 @@ export default {
   },
   mounted() {},
   created() {
-    fetch('https://ipecho.net/plain', {
+    fetch('https://consent-api.hdc.ntnu.no/ip4', {
       method: "GET",
       cache: "no-cache",
       headers: {
@@ -38,8 +38,8 @@ export default {
 <template>
   <div style="display: unset !important;">
     <strong><code v-if="show">{{ getMyIpaddress }}</code></strong>
-    <a href="http://ip4.me/" target="_blank">
+    <!-- <a href="http://ip4.me/" target="_blank">
       <span v-if="show"> [ip4.me]</span><span v-else>ip4.me</span><span><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg> <span class="sr-only">(opens new window)</span></span>
-    </a>
+    </a> -->
   </div>
 </template>
