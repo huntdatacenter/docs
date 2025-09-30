d36f76b400e730e4ee5a83b870054b7f88963df1
Matus Kosut <matus.kosut@ntnu.no>
2025-09-30 07:36:36 +0200

    cleanup api urls

--- docs/.vitepress/theme/components/price-estimator/api/pricesApi.js
+++ docs/.vitepress/theme/components/price-estimator/api/pricesApi.js
@@ -1,11 +1,17 @@
-// import data from './data.json';
+//
 import axios from "axios"
+
+const price_list_url = "https://assets.hdc.ntnu.no/assets/prices/v5/hunt-cloud-pricelist-v5-0.json"
+
+const gpu_models_url = "https://assets.hdc.ntnu.no/assets/js/gpus.json"
+
+const machine_flavors_url = "https://assets.hdc.ntnu.no/assets/js/flavors.json"
 
 export default {
   async getPriceList() {
     try {
-      const res = await axios.get("https://assets.hdc.ntnu.no/assets/prices/v5/hunt-cloud-pricelist-v5-0.json")
-      console.log("Fetched price list:", res.data)
+      const res = await axios.get(price_list_url)
+      // console.log("Fetched price list:", res.data)
       return res.data
     } catch (error) {
       console.error("Error fetching price list:", error)
@@ -14,7 +20,7 @@ export default {
   },
   async getAvailableGPUS() {
     try {
-      const res = await axios.get("https://assets.hdc.ntnu.no/assets/js/gpus.json")
+      const res = await axios.get(gpu_models_url)
       return res.data
     } catch (error) {
       console.error("Error fetching available gpus:", error)
@@ -23,7 +29,7 @@ export default {
   },
   async getMachineFlavors() {
     try {
-      const response = await axios.get("https://assets.hdc.ntnu.no/assets/js/flavors.json")
+      const response = await axios.get(machine_flavors_url)
       return response.data
     } catch (error) {
       console.error("Error fetching machine flavors:", error)
